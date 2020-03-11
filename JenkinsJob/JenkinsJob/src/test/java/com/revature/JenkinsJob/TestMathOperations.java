227db8c5fdfa7bf5f75a3f8365d7f6afe5aef50e
brnmcdnld720 <61257744+brnmcdnld720@users.noreply.github.com>
2020-03-11 11:57:13 -0400

    Initial commit

---
+++ JenkinsJob/JenkinsJob/src/test/java/com/revature/JenkinsJob/TestMathOperations.java
@@ -0,0 +1,33 @@
+package com.revature.JenkinsJob;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import com.revature.JenkinsJob.*;
+
+public class TestMathOperations {
+	int x,y;
+	   @BeforeEach
+	  public void setUp() throws Exception { 
+		  x=4;y=5;
+		  System.out.println("before");
+	  }
+	@DisplayName("Testing the Add Method...")
+	@Test
+	public void testAdd() {
+		System.out.println("Testing add . . . ");
+		int result = MathOperations.add(x,y);
+		System.out.println(result);
+		assertTrue(result == 9);
+	}
+	@DisplayName("Testing the Sub Method...")
+	@Test
+	public void testSub() {
+		System.out.println("Testing Sub . . . ");
+		int result = MathOperations.subtract(x,y);
+		System.out.println(result);
+		assertTrue(result == -1);
+	}
+}
